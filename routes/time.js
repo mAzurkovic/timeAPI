a2973a1a9329f0a38e647ec1b932a34d62270f49
Mattias Zurkovic <mattias@zurkovic.com>
2017-08-14 03:44:39 -0700

    init

---
+++ routes/time.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/:query', function(req, res, next) {
+  let unixTime = ''
+  let formattedTime = ''
+
+  if ( !req.params.query.match(/[a-z]/i) ) {
+    unixTime = req.params.query
+  } else {
+    let time = new Date(req.params.query * 1000)
+    formatted = time.toISOString()
+  }
+  res.send(JSON.stringify( {
+    'unix': unixTime,
+    'formatted': formatted
+  } ))
+});
+
+module.exports = router;
