c475451ff6f373f3b49b8bc60e0d8037e273f2af
Mattias Zurkovic <mattias@zurkovic.com>
2017-08-15 05:46:46 -0700

    add natural date processing

--- routes/time.js
+++ routes/time.js
@@ -4,20 +4,24 @@ var router = express.Router();
 
 router.get('/:query', function(req, res, next) {
   let unixTime = ''
-  let formattedTime = ''
+  let formatted = ''
+  let natural = ''
 
   if ( !req.params.query.match(/[a-z]/i) ) {
-    unixTime = req.params.query
+    unixTime = Number(req.params.query)
     formatted = moment.unix(unixTime).format("MM/DD/YYYY");
-
-    res.send({
-      unix: unixTime,
-      formatted: formatted
-    })
-
-  } else {
-    res.send(req.params.query)
+    natural = moment.unix(unixTime).format("MMMM Do YYYY");
+  } else { // has letters, ie natural language date
+    unixTime = Date.parse(req.params.query)/1000
+    formatted = moment.unix(unixTime).format("MM/DD/YYYY");
+    natural = moment.unix(unixTime).format("MMMM Do YYYY");
   }
+
+  res.send({
+    unix: unixTime,
+    natural: natural,
+    formatted: formatted
+  })
 
 });
 
